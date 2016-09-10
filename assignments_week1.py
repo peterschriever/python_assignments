10f7db3ac62f7c6df55594665cc658b559b30832
Peter Schriever <peterzen72@gmail.com>
2016-09-10 15:40:16 +0200

    Start of week 1 assignments

---
+++ assignments_week1.py
@@ -0,0 +1,25 @@
+# Assignment 1.A
+print("Assignment 1.A")
+numericalList = [2, 3, 4]
+print(numericalList)
+
+print("\n")
+
+# Assignment 1.B
+print("Assignment 1.B")
+wordList = ["red", "green", "blue"]
+print(wordList)
+
+print("\n")
+
+# Assignment 1.C
+print("Assignment 1.C")
+numericalListUsingRange = list(range(3, 6))
+print(numericalListUsingRange)
+
+print("\n")
+
+# Asssignment 1.D
+print("Assignment 1.D")
+characterList = ['a', 'b', 'c', 'd']
+print(characterList);
