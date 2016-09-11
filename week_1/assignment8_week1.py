bcae29ead6a9557945c2f5656adef50dfb696846
Peter Schriever <peterzen72@gmail.com>
2016-09-11 12:12:47 +0200

    Minor code updates to assignment 8

--- week_1/assignment8_week1.py
+++ week_1/assignment8_week1.py
@@ -13,17 +13,17 @@ print("Welcome to the RealFeeling Temperature Calculator!")
 print("Please type the temperature in Celsius: ")
 while True:
     inputTemp = input("[-100 - 100]: ")
-    fltInputTemp = stringToIntIfPossible(inputTemp)
-    if fltInputTemp is not False:
+    intInputTemp = stringToIntIfPossible(inputTemp)
+    if intInputTemp is not False:
         break
 
 print("Please type a number of windforce on the Scale of Beaufort: ")
 while True:
     inputWindforce = input("[0 - 9]: ")
-    fltWindforce = stringToIntIfPossible(inputWindforce)
-    if fltWindforce is not False:
+    intWindforce = stringToIntIfPossible(inputWindforce)
+    if intWindforce is not False:
         break
 
 print("The RealFeeling Temperature is: ",
- calculateWindchill(fltInputTemp, fltWindforce),
+ calculateWindchill(intInputTemp, intWindforce),
  " degrees Celsius")
