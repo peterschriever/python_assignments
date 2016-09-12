b73e1a8db16870c12c38915b2bdb52c56c2041af
Peter Schriever <peterzen72@gmail.com>
2016-09-12 13:48:08 +0200

    Finished assignment 9

--- week_1/assignment9_week1.py
+++ week_1/assignment9_week1.py
@@ -1,12 +1,24 @@
 def mergeSortedNumericLists(l1, l2):
-    mergedList = l1 + l2
-    return mergedList
+    longList = l1 if len(l1) > len(l2) else l2
+    shortList = l2 if len(l1) > len(l2) else l1
+
+    llPosition = 0
+    for numberA in longList:
+        slPosition = 0
+        for numberB in shortList:
+            if numberB < numberA:
+                # extend the longList, reduce the shortList
+                # the longList will be our sortedMergedList in the end
+                longList.insert(llPosition, shortList.pop(slPosition))
+            slPosition = slPosition + 1
+        llPosition = llPosition + 1
+        if len(shortList) == 0 :
+            break # we have succesfully merged
+    return longList
 
 print("Welcome to the SortedNumericList Combiner!")
 l1 = [1, 5, 16, 61, 111]; l2 = [2, 4, 5, 6]
 print("List1 : ", l1)
 print("List2 : ", l2)
-print("Expected result: ", [1, 2, 6, 16, 61, 77]) # this is weird
-# TODO: finish this assignment after clarification by the teacher.
-
+print("Expected result: ", [1, 2, 4, 5, 6, 16, 61, 111])
 print("Actual result: ", mergeSortedNumericLists(l1 ,l2))
